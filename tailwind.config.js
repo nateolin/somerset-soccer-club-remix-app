18778cd017fa825481d72cf37db513bc54230a0e
thenolin <nateolin17@gmail.com>
2022-02-28 11:20:38 -0600

    configure tailwind with postcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+const fromRoot = (p) => path.join(__dirname, p);
+
 module.exports = {
-  content: ["./src/**/*.{html,js,ts}"],
+  content: ["./src/**/*.{html,js,ts,tsx,jsx}"],
   theme: {
     extend: {},
   },
