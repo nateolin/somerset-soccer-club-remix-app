daecdd3ca4dae894b8518f85f3d0b371dae96ee9
thenolin <nateolin17@gmail.com>
2022-03-03 12:56:05 -0600

    refactor root to handle errors & add links

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,82 @@
 import {
   Links,
+  LinksFunction,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from "remix";
 import type { MetaFunction } from "remix";
+import tailwindStyles from './styles/tailwind.css'
+
+export const links: LinksFunction = () => {
+  return [
+    {rel: 'stylesheet', href: tailwindStyles}
+  ]
+}
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
 };
 
-export default function App() {
+function Document({
+  children,
+  title = `Somerset Soccer Club`,
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link href="/dist/global.css" rel="stylesheet" />
         <Meta />
+        <title>{title}</title>
         <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
+        {children}
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+      <ScrollRestoration/>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <div className="error-container">
+        <h1>
+          {caught.status} {caught.statusText}
+        </h1>
+      </div>
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
+  return (
+    <Document title="Uh-oh!">
+      <div className="error-container">
+        <h1>App Error</h1>
+        <pre>{error.message}</pre>
+      </div>
+    </Document>
   );
 }
